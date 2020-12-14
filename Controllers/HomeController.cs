fb0e8481b5e5f0656325df6efe0f713707504a0d
jbanos2000 <71935605+jbanos2000@users.noreply.github.com>
2020-12-14 17:35:39 -0500

    Modificando con BD

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,20 +6,23 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using PC_03.Models;
-
+using PC_03.Data;
 namespace PC_03.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            var productos = _context.Productos.ToList();
             return View();
         }
          public IActionResult Producto()
@@ -31,6 +34,8 @@ namespace PC_03.Controllers
         public IActionResult Producto(Producto p)
         {
             if(ModelState.IsValid){
+                _context.Add(p);
+                _context.SaveChanges();
                 return RedirectToAction("Index");
             }
             return View(p);
@@ -38,6 +43,15 @@ namespace PC_03.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+         [HttpPost]
+        public IActionResult Eliminar(int id)
+        {
+            var productos = _context.Productos.FirstOrDefault(p => p.id == id);
+            _context.Remove(productos);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
